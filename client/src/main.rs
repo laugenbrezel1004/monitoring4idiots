8aab024c6c8074c45f22aa726e76b27dacf279e6
laurenz <laurenzschmidt214@gmail.com>
2025-07-09 17:03:14 +0200

    logging, errorhandeling

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,20 @@
-mod systeminfo;
-mod daemon;
+use crate::daemon::run_daemon;
+use log::error;
+
 mod core;
+mod daemon;
+mod systeminfo;
 
 fn main() {
+    // Initialisiere Logging
+    if let Err(e) = core::init_logging() {
+        eprintln!("Failed to initialize logging: {}", e);
+        std::process::exit(1);
+    }
 
-    if let Err(e)  = daemon::run_daemon() {
-        eprintln!("error running daemon: {:?}", e);
+    // Starte den Daemon
+    if let Err(e) = run_daemon() {
+        error!("Daemon failed: {}", e);
+        std::process::exit(1);
     }
 }
-
